600da60f30822e1d3b0ecd4c17086a327c524069
fangting <fangting1989@126.com>
2017-07-18 14:19:55 +0800

    提交

--- controllers/gdxmController.go
+++ controllers/gdxmController.go
@@ -6,6 +6,7 @@ import (
 	"../models"
 	"../utils"
 	"github.com/gin-gonic/gin"
+	"github.com/go-xorm/xorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
@@ -13,7 +14,7 @@ func RegistergdxmRoutes(route *gin.Engine) {
 	gdxmController := route.Group("gdxmController")
 	{
 		gdxmController.POST("/insert", gdxmInsert)
-		gdxmController.GET("/update", gdxmUpdate)
+		gdxmController.POST("/update", gdxmUpdate)
 		gdxmController.GET("/del", gdxmDel)
 		gdxmController.GET("/find", gdxmFind)
 		gdxmController.GET("/test", gdxmTest)
@@ -22,76 +23,97 @@ func RegistergdxmRoutes(route *gin.Engine) {
 
 /*对象查询*/
 func gdxmFind(c *gin.Context) {
+	engine, err := xorm.NewEngine("mysql", "root:123456@192.168.2.66/landanalysis?charset=utf8")
+	utils.Check(err)
 	var pageobject utils.Pageobject
 	if c.Bind(&pageobject) == nil {
-		c.JSON(http.StatusOK, gin.H{"errid": 1, errmsg: ""})
-		// if pageobject.pagenum == nil {
-		// 	pageobject.pagenum = 1
-		// }
-		// if pageobject.pagesize == nil {
-		// 	pageobject.pagesize = 10
-		// }
-		// var gdxm models.Gdxm
-		// gdxms := make([]gdxm, 0)
-		// /*书写对应逻辑*/
+		if pageobject.Pagenum == 0 {
+			pageobject.Pagenum = 1
+		}
+		if pageobject.Pagesize == 0 {
+			pageobject.Pagesize = 10
+		}
+		var gdxm models.Gdxm
+		gdxms := make([]models.Gdxm, 0)
+		/*书写对应逻辑*/
 
-		// err := engine.Where("1 = 1").Limit(pageobject.pagesize, pageobject.pagesize*(pageobject.pagenum-1)).Find(&gdxms)
-		// if err == nil {
-		// 	c.JSON(http.StatusOK, gin.H{"error": "not find data"})
-		// 	return
-		// }
-		// total, err := engine.Where("1 = 1").Count(gdxm)
-		// if err == nil {
-		// 	c.JSON(http.StatusOK, gin.H{"error": "not find data"})
-		// 	return
-		// }
-		// c.JSON(http.StatusOK, gin.H{"errid": 1, errmsg: "", data: gdxms, recordcount: total})
+		err := engine.Where("1 = 1").Limit(pageobject.Pagesize, pageobject.Pagesize*(pageobject.Pagenum-1)).Find(&gdxms)
+		if err == nil {
+			c.JSON(http.StatusOK, gin.H{"error": "not find data"})
+			return
+		}
+		total, err := engine.Where("1 = 1").Count(gdxm)
+		if err == nil {
+			c.JSON(http.StatusOK, gin.H{"error": "not find data"})
+			return
+		}
+		c.JSON(http.StatusOK, &utils.ResultList{1, "", gdxms, total})
 	}
-
 }
 
 /*对象插入*/
 func gdxmInsert(c *gin.Context) {
+	engine, err := xorm.NewEngine("mysql", "root:123456@192.168.2.66/landanalysis?charset=utf8")
+	utils.Check(err)
 	//insert
 	var gdxm models.Gdxm
-	// if c.Bind(&gdxm) == nil { BindJSON
 	if c.BindJSON(&gdxm) == nil {
 		affected, err := engine.Insert(gdxm)
-		c.JSON(http.StatusOK, gdxm)
+		utils.Check(err)
+		if affected > 0 {
+			c.JSON(http.StatusOK, &utils.ResultObject{1, "插入成功", gdxm})
+		} else {
+			c.JSON(http.StatusOK, &utils.ResultObject{-1, "插入失败", gdxm})
+		}
 	} else {
-		c.JSON(http.StatusOK, gin.H{"errid": -1, errmsg: "no data", data: nil})
+		c.JSON(http.StatusOK, &utils.ResultObject{-1, "参数解析错误", nil})
 	}
 }
 
 /*对象更新*/
 func gdxmUpdate(c *gin.Context) {
+	engine, err := xorm.NewEngine("mysql", "root:123456@192.168.2.66/landanalysis?charset=utf8")
+	utils.Check(err)
 	var gdxm models.Gdxm
 	if c.BindJSON(&gdxm) == nil {
 		affected, err := engine.Id(gdxm.GDXM_ID).Update(gdxm)
-		c.JSON(http.StatusOK, gin.H{"errid": 1, errmsg: "更新成功", data: gdxm})
+		utils.Check(err)
+		if affected > 0 {
+			c.JSON(http.StatusOK, &utils.ResultObject{1, "更新成功", gdxm})
+		} else {
+			c.JSON(http.StatusOK, &utils.ResultObject{-1, "更新失败", gdxm})
+		}
 	} else {
-		c.JSON(http.StatusOK, gin.H{"errid": -1, errmsg: "no data", data: nil})
+		c.JSON(http.StatusOK, &utils.ResultObject{-1, "参数解析错误", nil})
 	}
 }
 
 /*对象删除*/
 func gdxmDel(c *gin.Context) {
+	engine, err := xorm.NewEngine("mysql", "root:123456@192.168.2.66/landanalysis?charset=utf8")
+	utils.Check(err)
 	var gdxm models.Gdxm
 	if c.Bind(&gdxm) == nil {
 		affected, err := engine.Id(gdxm.GDXM_ID).Delete(gdxm)
-		c.JSON(http.StatusOK, gin.H{"errid": 1, errmsg: "删除成功", data: nil})
+		utils.Check(err)
+		if affected > 0 {
+			c.JSON(http.StatusOK, &utils.ResultObject{1, "删除成功", nil})
+		} else {
+			c.JSON(http.StatusOK, &utils.ResultObject{-1, "删除失败", nil})
+		}
+
 	} else {
-		c.JSON(http.StatusOK, gin.H{"errid": -1, errmsg: "no data", data: nil})
+		c.JSON(http.StatusOK, &utils.ResultObject{-1, "参数解析错误", nil})
 	}
 }
 
 func gdxmTest(c *gin.Context) {
 	var gdxm models.Gdxm
 	if c.Bind(&gdxm) == nil {
-		utils.StructInfo(gdxm)
-		c.JSON(http.StatusOK, gin.H{"errid": -1, errmsg: "has data", data: nil})
+		utils.StructInfo(&gdxm)
+		c.JSON(http.StatusOK, &utils.ResultList{10, "123", gdxm, 10})
 	} else {
-		c.JSON(http.StatusOK, gin.H{"errid": -1, errmsg: "no data", data: nil})
+		c.JSON(http.StatusOK, &utils.ResultObject{-1, "参数解析错误", nil})
 	}
 
 }
--- utils/global.go
+++ utils/global.go
@@ -6,34 +6,49 @@ import (
 )
 
 type Pageobject struct {
-	pagenum  int64
-	pagesize int64
-	// pagenum  int64 `xorm:"BIGINT(20)"`
-	// pagesize int64 `xorm:"BIGINT(20)"`
+	Pagenum  int
+	Pagesize int
+}
+
+type ResultList struct {
+	Errid       int
+	Errmsg      string
+	Data        interface{}
+	Recordcount int64
+}
+
+type ResultObject struct {
+	Errid  int
+	Errmsg string
+	Data   interface{}
 }
 
 func StructInfo(obj interface{}) string {
-	t := reflect.ValueOf(obj)
+	val := reflect.ValueOf(obj).Elem()
 	// fmt.Println("type:", t.Name())
 
-	if t.Kind() != reflect.Struct {
-		fmt.Println("err:type invalid")
-	}
-	// t.Elem().NumField()
+	// if t.Kind() != reflect.Struct {
+	// 	fmt.Println("err:type invalid")
+	// }
+	// s := t.Elem()
+	// typeOfT := s.Type() //把s.Type()返回的Type对象复制给typeofT，typeofT也是一个反射
+	for k := 0; k < val.NumField(); k++ {
 
-	// v := reflect.ValueOf(o)
-	// u := T{Id: 1001, Name: "xxx" /*, addr:"xxx"*/}
-	// t := reflect.TypeOf(u)
-	// v := reflect.ValueOf(u)
-	s := t.Elem()
-	typeOfT := s.Type() //把s.Type()返回的Type对象复制给typeofT，typeofT也是一个反射
-	for k := 0; k < s.NumField(); k++ {
+		valueField := val.Field(k)
+		typeField := val.Type().Field(k)
+		tag := typeField.Tag
+
 		// var aa = 123
-		f := s.Field(k) //迭代s的各个域，注意每个域仍然是反射。
-		fmt.Printf("%d: %s %s = %v\n", k,
-			typeOfT.Field(k).Name, f.Type(), f.Interface()) //提取了每个域的名字
+		// f := val.Field(k)                                                                                                               //迭代s的各个域，注意每个域仍然是反射。
+		fmt.Printf("Field Name: %s,\t Field Value: %v,\t Tag Value: %s\n", typeField.Name, valueField.Interface(), tag.Get("tag_name")) //提取了每个域的名字
 
 		// fmt.Printf("%s -- %v \n", t.Filed(k).Name, v.Field(k).Interface())
 	}
 	return "123"
+}
+
+func Check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
